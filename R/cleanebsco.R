6bcf463dd2a06c19805ec93d6f9516268cd5b081
phoebehlam <62526368+phoebehlam@users.noreply.github.com>
2024-02-22 10:57:55 -0500

    smarter og cleanebsco()

--- R/cleanebsco.R
+++ R/cleanebsco.R
@@ -10,40 +10,50 @@
 cleanebsco<- function(path, name, windows=F) {
   
   if (windows == F) {
+  
+    openxlsx::read.xlsx(paste(path, "/", name, ".xlsx", sep=""), startRow = 2) %>% 
+      rename(article_ID = `/rec/@resultID`,
+             author = `/rec/header/controlInfo/artinfo/aug/au`,
+             title = `/rec/header/controlInfo/artinfo/tig/atl`,
+             journal = `/rec/header/controlInfo/jinfo/jtl`,
+             abstract = `/rec/header/controlInfo/artinfo/ab`,
+             year = `/rec/header/controlInfo/pubinfo/dt/@year`) %>%
+      select(article_ID, author, title, journal, abstract, year)-> dat
     
-    openxlsx::read.xlsx(paste(path, "/", name, ".xlsx", sep="")) %>%
-      dplyr::group_by(X2) %>%
-      tidyr::fill(X28, .direction = 'up') %>%
-      dplyr::filter(is.na(X7)==F) %>%
+    dat %>% 
+      dplyr::group_by(article_ID) %>%
+      tidyr::fill(title, .direction = 'down') %>% 
+      tidyr::fill(journal, .direction = 'up') %>% 
+      tidyr::fill(abstract, .direction = 'down') %>% 
+      dplyr::filter(is.na(author)==F) %>% 
       dplyr::filter(dplyr::row_number()==1) %>%
       dplyr::ungroup() %>%
-      dplyr::rename(article_ID = X2,
-                    first_author = X7,
-                    title = X15,
-                    year = X34,
-                    journal = X28) %>%
-      dplyr::select(article_ID, first_author, year, journal, title) %>%
+      dplyr::rename(first_author = author) %>%
+      dplyr::select(article_ID, first_author, year, journal, title, abstract) %>%
       dplyr::mutate(article_ID = suppressWarnings(as.numeric(article_ID)),
                     year = suppressWarnings(as.numeric(year))) %>%
       dplyr::filter(is.na(article_ID)==F)-> dat
     
   }else if(windows == T) {
     
-    openxlsx::read.xlsx(paste(path, "/", name, ".xlsx", sep="")) %>%
+    dat %>%
       dplyr::group_by(resultID) %>%
       tidyr::fill(atl, .direction = 'down') %>% 
       tidyr::fill(jtl, .direction = 'down') %>% 
+      tidyr::fill(ab, .direction = 'up') %>% 
       dplyr::filter(is.na(au5)==F) %>% 
       dplyr::filter(dplyr::row_number()==1) %>%
       dplyr::ungroup() %>%
       dplyr::rename(article_ID = resultID,
                     first_author = au5,
                     title = atl,
-                    journal = jtl) %>%
-      dplyr::select(article_ID, first_author, year, journal, title) %>%
+                    journal = jtl,
+                    abstract = ab) %>%
+      dplyr::select(article_ID, first_author, year, journal, title, abstract) %>%
       dplyr::mutate(article_ID = suppressWarnings(as.numeric(article_ID)),
                     year = suppressWarnings(as.numeric(year))) %>%
       dplyr::filter(is.na(article_ID)==F)-> dat
+    
     
   }
   
